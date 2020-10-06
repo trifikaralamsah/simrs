a72cde2aa997abeb11f2602f9ed6ce7f9a4031e6
trifikaralamsah <trifikar24@gmail.com>
2020-10-06 22:30:14 +0700

    commit kedua

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,24 +18,32 @@
         <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
         <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
-        <img src="{{ ('img/home/rs1.jpeg') }}" class="d-block w-100 " alt="Responsive image" style="height:500px">
+        <img src="{{ ('img/home/rs1.png') }}" class="d-block w-100 " alt="Responsive image" style="height:500px">
         <div class="carousel-caption d-none d-md-block">
             <h5>First slide label</h5>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </div>
         </div>
         <div class="carousel-item">
-        <img src="{{ ('img/home/rs1.png') }}" class="d-block w-100" alt="Responsive image" style="height:500px">
+        <img src="{{ ('img/home/rsr4.png') }}" class="d-block w-100" alt="Responsive image" style="height:500px">
         <div class="carousel-caption d-none d-md-block">
             <h5>Second slide label</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
         </div>
         <div class="carousel-item">
-        <img src="{{ ('img/home/rs3.jpeg') }}" class="d-block w-100" alt="Responsive image" style="height:500px">
+        <img src="{{ ('img/home/rsr2.png') }}" class="d-block w-100" alt="Responsive image" style="height:500px">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </div>
+        </div>
+        <div class="carousel-item">
+        <img src="{{ ('img/home/rsr3.png') }}" class="d-block w-100" alt="Responsive image" style="height:500px">
         <div class="carousel-caption d-none d-md-block">
             <h5>Third slide label</h5>
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
